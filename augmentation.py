72b79055d1df18140361d568afab0a019a61310b
Augusto Peiter <augusto.peiter@gmail.com>
2022-07-20 18:59:46 +0000

    Adding initial training code

---
+++ augmentation.py
@@ -0,0 +1,66 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from skimage.transform import AffineTransform, warp
+from skimage import io, img_as_ubyte
+import random
+import os
+import cv2
+from os import path
+from scipy.ndimage import rotate
+
+import albumentations as A
+images_to_generate=1000
+
+
+
+
+images_path="/content/TCC_Cell_Semantic_Segmentation/DIC-C2DH-HeLa/01" #path to original images
+masks_path = "/content/TCC_Cell_Semantic_Segmentation/DIC-C2DH-HeLa/01_ST"
+
+
+img_augmented_path="/content/TCC_Cell_Semantic_Segmentation/DIC-C2DH-HeLa/01_AUG" # path to store aumented images
+msk_augmented_path="/content/TCC_Cell_Semantic_Segmentation/DIC-C2DH-HeLa/01_ST_AUG" # path to store aumented images
+if path.exists(img_augmented_path) == False:
+  os.mkdir(img_augmented_path)
+if path.exists(msk_augmented_path) == False:
+  os.mkdir(msk_augmented_path)
+images=[] # to store paths of images from folder
+masks=[]
+
+for im in os.listdir(images_path):  # read image name from folder and append its path into "images" array     
+    images.append(os.path.join(images_path,im))
+
+for msk in os.listdir(masks_path):  # read image name from folder and append its path into "images" array     
+    masks.append(os.path.join(masks_path,msk))
+
+
+aug = A.Compose([
+    A.VerticalFlip(p=0.5),              
+    A.RandomRotate90(p=0.5),
+    A.HorizontalFlip(p=1),
+    A.Transpose(p=1),
+    #A.ElasticTransform(p=1, alpha=120, sigma=120 * 0.05, alpha_affine=120 * 0.03),
+    A.GridDistortion(p=1)
+    ]
+)
+
+for i in range(0, images_to_generate):
+    image = images[i % len(images)]
+    mask = masks[i % len(images)]
+    print(image, mask)
+    original_image = io.imread(image)
+    original_mask = io.imread(mask)
+    
+    augmented = aug(image=original_image, mask=original_mask)
+    transformed_image = augmented['image']
+    transformed_mask = augmented['mask']
+
+     
+    os.chdir(images_path)   
+    new_image_name = "augmented_image_%s.png" %(i)
+    os.chdir(masks_path)   
+    new_mask_name = "augmented_mask_%s.png" %(i)
+    cv2.imwrite(new_image_name, transformed_image)
+    cv2.imwrite(new_mask_name, transformed_mask)
+    i =i+1
+    
---
+++ train.py
@@ -0,0 +1,122 @@
+
+"""
+Segmentation MOdels library info:
+#https://github.com/qubvel/segmentation_models
+#Recommended for colab execution
+TensorFlow ==2.1.0
+keras ==2.3.1
+pip install segmentation-models
+For this demo it is working on a local workstation...
+Python 3.5
+TensorFlow ==1.
+keras ==2
+"""
+
+import tensorflow as tf
+import segmentation_models as sm
+import glob
+import cv2
+import os
+from os import path
+import numpy as np
+from matplotlib import pyplot as plt
+
+BACKBONE = 'resnet34'
+preprocess_input = sm.get_preprocessing(BACKBONE)
+
+
+results_path="/content/TCC_Cell_Semantic_Segmentation/DIC-C2DH-HeLa/Results" # path to store model results
+if path.exists(results_path) == False:
+  os.mkdir(results_path)
+
+#Resizing images is optional, CNNs are ok with large images
+SIZE_X = 256 #Resize images (height  = X, width = Y)
+SIZE_Y = 256
+
+#Capture training image info as a list
+train_images = []
+
+for directory_path in glob.glob("/content/TCC_Cell_Semantic_Segmentation/DIC-C2DH-HeLa/01_AUG"):
+    for img_path in glob.glob(os.path.join(directory_path, "*.png")):
+        #print(img_path)
+        img = cv2.imread(img_path, cv2.IMREAD_COLOR)       
+        #img = cv2.resize(img, (SIZE_Y, SIZE_X))
+        #img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+        train_images.append(img)
+        #train_labels.append(label)
+#Convert list to array for machine learning processing        
+train_images = np.array(train_images)
+
+#Capture mask/label info as a list
+train_masks = [] 
+for directory_path in glob.glob("/content/TCC_Cell_Semantic_Segmentation/DIC-C2DH-HeLa/01_ST_AUG"):
+    for mask_path in glob.glob(os.path.join(directory_path, "*.png")):
+        mask = cv2.imread(mask_path, 0)       
+        #mask = cv2.resize(mask, (SIZE_Y, SIZE_X))
+        #mask = cv2.cvtColor(mask, cv2.COLOR_RGB2BGR)
+        train_masks.append(mask)
+        #train_labels.append(label)
+#Convert list to array for machine learning processing          
+train_masks = np.array(train_masks)
+
+#Use customary x_train and y_train variables
+X = train_images
+Y = train_masks
+#Y = np.expand_dims(Y, axis=3) #May not be necessary.. leftover from previous code 
+
+
+from sklearn.model_selection import train_test_split
+x_train, x_val, y_train, y_val = train_test_split(X, Y, test_size=0.2, random_state=42)
+
+# preprocess input
+x_train = preprocess_input(x_train)
+x_val = preprocess_input(x_val)
+
+# define model
+model = sm.Unet(BACKBONE, encoder_weights='imagenet')
+model.compile(optimizer='Adam', loss=sm.losses.bce_jaccard_loss, metrics=[sm.metrics.iou_score])
+
+print(model.summary())
+
+
+history=model.fit(x_train, 
+          y_train,
+          batch_size=32, 
+          epochs=1000,
+          verbose=1,
+          validation_data=(x_val, y_val))
+
+
+
+#accuracy = model.evaluate(x_val, y_val)
+#plot the training and validation accuracy and loss at each epoch
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+epochs = range(1, len(loss) + 1)
+plt.plot(epochs, loss, 'y', label='Training loss')
+plt.plot(epochs, val_loss, 'r', label='Validation loss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
+
+model.save('membrane.h5')
+os.chdir(results_path)
+
+from tensorflow import keras
+model = keras.models.load_model('membrane.h5', compile=False)
+#Test on a different image
+#READ EXTERNAL IMAGE...
+test_img = cv2.imread('/content/TCC_Cell_Semantic_Segmentation/DIC-C2DH-HeLa/02/t000.tif', cv2.IMREAD_UNCHANGED)       
+#test_img = cv2.resize(test_img, (SIZE_Y, SIZE_X))
+test_img = cv2.cvtColor(test_img, cv2.COLOR_BGR2RGB)
+test_img = np.expand_dims(test_img, axis=0)
+
+prediction = model.predict(test_img)
+
+#View and Save segmented image
+prediction_image = prediction.reshape(mask.shape)
+plt.imshow(prediction_image, cmap='gray')
+output_path = results_path + 'test0_segmented.jpg'
+plt.imsave(output_path, prediction_image, cmap='gray')
