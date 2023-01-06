21cfefc50a2917fd4a63267e44cd9a10759c114d
Augusto Peiter <augusto.peiter@gmail.com>
2023-01-06 01:07:20 +0000

    Part 6 Change model to efficient unet ++ unfinished, train fn + metrics and eval taken from git https://github.com/jlcsilva/EfficientUNetPlusPlus/tree/a68471d921426219b22bba1077f46869f9515e0d

--- train.py
+++ train.py
@@ -1,15 +1,26 @@
 #import tensorflow as tf
+import logging
 import segmentation_models_pytorch as sm
 import glob
 import cv2
 import os
 from os import path
 import numpy as np
-from matplotlib import pyplot as plt
 import albumentations as A
 import torch
+import torch.nn as nn
+
+from torch import optim
+from tqdm import tqdm
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
 from sklearn.model_selection import train_test_split
-#from keras.utils import to_categorical
+from torch.utils.tensorboard import SummaryWriter
+from kornia.losses import focal_loss
+from matplotlib import pyplot as plt
+
+from metrics import dice_loss
+from eval import eval_net
 from utils import (
   save_checkpoint,
   load_checkpoint,
@@ -26,6 +37,7 @@ TRAIN_IMG_DIRS = ['/content/TCC_Cell_Semantic_Segmentation/Fluo-C2DL-MSC/01', '/
 TRAIN_MASK_DIRS = ['/content/TCC_Cell_Semantic_Segmentation/Fluo-C2DL-MSC/01_ST/SEG', '/content/TCC_Cell_Semantic_Segmentation/Fluo-N2DH-GOWT1/01_ST/SEG']
 BATCH_SIZE = 32
 EPOCHS = 25
+LR = 0.001
 LOAD_MODEL = False
 IMAGE_WIDTH = 256
 IMAGE_HEIGHT = 256
@@ -34,68 +46,155 @@ LOSS = sm.losses.DiceLoss
 METRICS = "accuracy"#sm.metrics.iou_score
 BACKBONE = 'timm-efficientnet-b0'
 ENCODER_WEIGHTS = 'imagenet'
-IMAGES_TO_GENERATE = 300
+AUGMENTATION_PER_IMAGE = 8
 TRAIN_SPLIT_SIZE = 0.8
 TEST_IMG = '/content/TCC_Cell_Semantic_Segmentation/Fluo-C2DL-MSC/02/t000.tif'
 RESULTS_PATH ="/content/TCC_Cell_Semantic_Segmentation/Results" # path to store model results
 NUM_CLASSES = 2
 ACTIVATION = "softmax"
 
-def train_fn(loader, model):
-  preprocess_input = sm.get_preprocessing(BACKBONE, ENCODER_WEIGHTS)
+def   train_fn(model, 
+            device,
+            training_set,
+            validation_set,
+            dir_checkpoint,
+            epochs=50,
+            batch_size=2,
+            lr=0.001,
+            save_cp=True,
+            img_scale=1,
+            n_classes=2,
+            n_channels=3,
+            augmentation_ratio = 8):
 
-  X, Y = loader.__get_img_mask_list__(height=IMAGE_HEIGHT, width=IMAGE_WIDTH)
-  Y = np.expand_dims(Y, axis=3) #May not be necessary.. leftover from previous code 
+  train = training_set 
+  val = validation_set
+  n_train = len(train)
+  n_val = len(val)
+  train_loader = DataLoader(train, batch_size=batch_size, shuffle=True, num_workers=8, pin_memory=True)
+  val_loader = DataLoader(val, batch_size=batch_size, shuffle=False, num_workers=8, pin_memory=True)
 
-  
-  x_train, x_val, y_train, y_val = train_test_split(X, Y, test_size=TRAIN_SPLIT_SIZE, random_state=42)
 
-  #y_train = to_categorical(y_train, num_classes=NUM_CLASSES)
-  #y_val = to_categorical(y_val, num_classes=NUM_CLASSES)
+  # Sets the effective batch size according to the batch size and the data augmentation ratio
+  batch_size = (1 + augmentation_ratio)*batch_size
 
-  # preprocess input
-  x_train = preprocess_input(x_train)
-  x_val = preprocess_input(x_val)
+  # Prepares the summary file
+  writer = SummaryWriter(comment=f'LR_{lr}_BS_{batch_size}_SCALE_{img_scale}')
+  global_step = 0
 
-  print(model.summary())
-  history=model.fit(x_train, 
-            y_train,
-            batch_size=BATCH_SIZE, 
-            epochs=EPOCHS,
-            verbose=1,
-            validation_data=(x_val, y_val))
+  logging.info(f'''Starting training:
+      Epochs:          {epochs}
+      Batch size:      {batch_size}
+      Learning rate:   {lr}
+      Training size:   {n_train}
+      Validation size: {n_val}
+      Checkpoints:     {save_cp}
+      Device:          {device.type}
+      Images scaling:  {img_scale}
+      Augmentation ratio: {augmentation_ratio}
+  ''')
 
-  #accuracy = model.evaluate(x_val, y_val)
-  #plot the training and validation accuracy and loss at each epoch
-  loss = history.history['loss']
-  val_loss = history.history['val_loss']
-  epochs = range(1, len(loss) + 1)
-  plt.plot(epochs, loss, 'y', label='Training loss')
-  plt.plot(epochs, val_loss, 'r', label='Validation loss')
-  plt.title('Training and validation loss')
-  plt.xlabel('Epochs')
-  plt.ylabel('Loss')
-  plt.legend()
-  plt.show()
-  return model
+  # Choose the optimizer and scheduler 
+  optimizer = optim.Adam(model.parameters(), lr=lr)
+  scheduler = optim.lr_scheduler.StepLR(optimizer, epochs//3, gamma=0.1, verbose=True)
 
-def predict(model, image_path):
-  image = cv2.imread(image_path, cv2.IMREAD_UNCHANGED)       
-  image = cv2.resize(image, (IMAGE_WIDTH, IMAGE_HEIGHT))
-  image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-  image = np.expand_dims(image, axis=0)
-  prediction = model.predict(image)
-  #View and Save segmented image  
-  prediction_image = np.argmax(prediction, axis=3)[0,:,:]
-  #prediction_image = prediction.reshape([256,256])
-  #new_img = cv2.cvtColor(prediction_image, cv2.COLOR_BGR2RGB)
-  plt.imshow(prediction_image, cmap='gray')
-  prediction_image_name = 'test_' + os.path.basename(image_path[:-4]) + '.png' 
-  output_path = os.path.join(RESULTS_PATH, prediction_image_name)
-  plt.imsave(output_path, prediction_image, cmap='gray')
+  # Train loop
+  for epoch in range(epochs):
+      model.train()
+      epoch_loss = 0
+      with tqdm(total=n_train, desc=f'Epoch {epoch + 1}/{epochs}', unit='img') as pbar:
+          for batch in train_loader:
+              imgs = batch['image']
+              true_masks = batch['mask']
+              # DataLoaders return lists of tensors. TODO: Concatenate the lists inside the DataLoaders
+              imgs = torch.cat(imgs, dim = 0)
+              true_masks = torch.cat(true_masks, dim = 0)
+
+              assert imgs.shape[1] == n_channels, \
+                  f'Network has been defined with {n_channels} input channels, ' \
+                  f'but loaded images have {imgs.shape[1]} channels. Please check that ' \
+                  'the images are loaded correctly.'
+
+              imgs = imgs.to(device=device, dtype=torch.float32)
+              mask_type = torch.float32 if n_classes == 1 else torch.long
+              true_masks = true_masks.to(device=device, dtype=mask_type)
+
+              masks_pred = model(imgs)                         
+              
+              # Compute loss
+              loss = focal_loss(masks_pred, true_masks.squeeze(1), alpha=0.25, gamma = 2, reduction='mean').unsqueeze(0)
+              loss += dice_loss(masks_pred, true_masks.squeeze(1), True, k = 0.75)
+
+              epoch_loss += loss.item()
+              writer.add_scalar('Loss/train', loss.item(), global_step)
+              pbar.set_postfix(**{'loss (batch)': loss.item()})
+
+              optimizer.zero_grad()
+              loss.backward()
+              nn.utils.clip_grad_value_(model.parameters(), 0.1)
+              optimizer.step()
+
+              pbar.update(imgs.shape[0]//(1 + augmentation_ratio))
+              global_step += 1
+              if global_step % (n_train // (batch_size / (1 + augmentation_ratio))) == 0:
+                  for tag, value in model.named_parameters():
+                      tag = tag.replace('.', '/')
+                      
+                      try:
+                          writer.add_histogram('weights/' + tag, value.data.cpu().numpy(), global_step)
+                          writer.add_histogram('grads/' + tag, value.grad.data.cpu().numpy(), global_step)
+                      except:
+                          pass
+                  
+                  epoch_score = eval_net(model, train_loader, device)
+                  val_score = eval_net(model, val_loader, device)
+                  writer.add_scalar('learning_rate', optimizer.param_groups[0]['lr'], global_step)
+
+                  if n_classes > 1:
+                      logging.info('Validation loss: {}'.format(val_score))
+                      writer.add_scalar('Generalized dice loss/train', epoch_score, global_step)
+                      writer.add_scalar('Generalized dice loss/test', val_score, global_step)
+                  else:
+                      logging.info('Validation loss: {}'.format(val_score))
+                      writer.add_scalar('Dice loss/train', epoch_score, global_step)
+                      writer.add_scalar('Dice loss/test', val_score, global_step)       
+      scheduler.step()         
+      if save_cp:
+          try:
+              os.mkdir(dir_checkpoint)
+              logging.info('Created checkpoint directory')
+          except OSError:
+              pass
+          torch.save(model.state_dict(),
+                      dir_checkpoint + f'CP_epoch{epoch + 1}.pth')
+          logging.info(f'Checkpoint {epoch + 1} saved !')
+  writer.close()
+
+
+# def predict(model, image_path):
+#   image = cv2.imread(image_path, cv2.IMREAD_UNCHANGED)       
+#   image = cv2.resize(image, (IMAGE_WIDTH, IMAGE_HEIGHT))
+#   image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+#   image = np.expand_dims(image, axis=0)
+#   prediction = model.predict(image)
+#   #View and Save segmented image  
+#   prediction_image = np.argmax(prediction, axis=3)[0,:,:]
+#   #prediction_image = prediction.reshape([256,256])
+#   #new_img = cv2.cvtColor(prediction_image, cv2.COLOR_BGR2RGB)
+#   plt.imshow(prediction_image, cmap='gray')
+#   prediction_image_name = 'test_' + os.path.basename(image_path[:-4]) + '.png' 
+#   output_path = os.path.join(RESULTS_PATH, prediction_image_name)
+#   plt.imsave(output_path, prediction_image, cmap='gray')
 
 
 def main():
+
+  logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
+
+  # Determine device
+  device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+  logging.info(f'Using device {device}')
+
   train_transform = A.Compose([
       A.VerticalFlip(p=0.5),              
       A.RandomRotate90(p=1),
@@ -115,8 +214,6 @@ def main():
       #A.RandomCrop(height=50, width=50, p=0.5)
       ]
   )
-  if os.path.exists(RESULTS_PATH) == False:
-    os.mkdir(RESULTS_PATH)
   if os.path.exists(MAIN_IMAGE_DIR):
     os.rmdir(MAIN_IMAGE_DIR)
   os.mkdir(MAIN_IMAGE_DIR)
@@ -145,7 +242,22 @@ def main():
     )
   # train_ds.__apply__(IMAGES_TO_GENERATE)
   # train_ds.__read_augmented__()
-  # model = sm.EfficientUnetPlusPlus(BACKBONE, encoder_weights=ENCODER_WEIGHTS, classes=NUM_CLASSES, activation=ACTIVATION)
+  model = sm.EfficientUnetPlusPlus(BACKBONE, encoder_weights=ENCODER_WEIGHTS, classes=NUM_CLASSES, activation=ACTIVATION)
+  # Distribute training over GPUs
+  model = nn.DataParallel(model)
+  train_fn(model=model, 
+            device=device,
+            training_set=train_ds,
+            validation_set=val_ds,
+            dir_checkpoint=RESULTS_PATH,
+            epochs=EPOCHS,
+            batch_size=BATCH_SIZE,
+            lr=LR,
+            save_cp=True,
+            img_scale=1,
+            n_classes=NUM_CLASSES,
+            n_channels=3,
+            augmentation_ratio = AUGMENTATION_PER_IMAGE)
   # model.compile(optimizer=OPTIMIZER, loss=LOSS, metrics=[METRICS])
   # model = train_fn(train_ds, model)
   # save_model_path = os.path.join(RESULTS_PATH, 'modelUNET.h5')
